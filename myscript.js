5c871683d17fe7dc7b5fab0e91e70772cafac36c
AlexJNeville <157894487+AlexJNeville@users.noreply.github.com>
2024-02-15 08:21:53 +0000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,14 @@
+document.addEventListener("scroll", function() {
+  var element = document.getElementById("element");
+  var trigger = document.getElementById("trigger");
+  var triggerPosition = trigger.getBoundingClientRect().top;
+  var scrollPosition = window.innerHeight;
 
+  if (triggerPosition < scrollPosition) {
+    element.classList.add("visible");
+    element.classList.remove("hidden");
+  } else {
+    element.classList.remove("visible");
+    element.classList.add("hidden");
+  }
+});
