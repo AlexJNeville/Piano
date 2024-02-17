1a3992520a9ac412ffecd7086ff7c4d2efce49b2
AlexJNeville <157894487+AlexJNeville@users.noreply.github.com>
2024-02-17 11:29:28 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-const boxes = document.querySelectorAll('.grid-container-back');
 
-window.addEventListener'scroll',checkBoxes);
-checkBoxes();
-function checkBoxes() {
-  const triggerBottom = window.innerHeight / 5 * 4;
-
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
-
-    if(boxTop < triggerBottom) {
-      box.classList.add('show');
-  
-    } else {
-      box.classList.remove('show');
-    }
-  }};
-}
