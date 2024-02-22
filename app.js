3afa3cb3ed3f870610bd7e79061d1505de55ad27
AlexJNeville <157894487+AlexJNeville@users.noreply.github.com>
2024-02-22 20:06:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
+const theanimation = document.querySelectorAll('.grid-container')
 
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('grid-container-animation')
+        }
+            else {
+                entry.target.classList.remove('grid-container-animation')
+            }
+        
+    })
+},
+   { threshold: 0.1
+   });
+//
+  for (let i = 0; i < theanimation.length; i++) {
+   const elements = theanimation[i];
+
+    observer1.observe(elements);
+  } 
+
+const the_animation = document.querySelectorAll('.grid-container-back')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('grid-container-back-animation')
+        }
+            else {
+                entry.target.classList.remove('grid-container-animation')
+            }
+        
+    })
+},
+   { threshold: 0.1
+   });
+//
+  for (let i = 0; i < the_animation.length; i++) {
+   const elements = the_animation[i];
+
+    observer.observe(elements);
+  } 
